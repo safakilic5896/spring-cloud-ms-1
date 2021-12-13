32100fdbd6d61a72ca63b7a9bd44d989f69f3c0d
safakilic5896 <lhystrikk@gmail.com>
2021-12-13 21:08:44 +0100

    feat(init)

---
+++ src/main/java/com/example/pocms1/PocMs1Application.java
@@ -0,0 +1,13 @@
+package com.example.pocms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMs1Application.class, args);
+	}
+
+}
